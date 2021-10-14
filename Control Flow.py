bd63c6e1b58fe759082ba948cc20487deb005751
Nolan <02Nolan.Loukes@gulllakecs.org>
2021-10-14 09:53:59 -0700

    Created a PIN variable

--- Control Flow.py
+++ Control Flow.py
@@ -8,8 +8,14 @@ Nesting If statements and refresh our Comparison & Logical Operators
 """
 
 print("Welcome to Cash-R-Us,\nLet's take a moment to set up your account.\n")
+
 # Set up account by asking Us Bank users for their first & last names using Variables
 firstName = input("What is your first name: ")
 lastName = input("What is your last name: ")
 
 print("\nWelcome to Cash-R-Us",firstName,lastName + ", we will now set up a security pin on you account.\n")
+
+# Set up PIN - Personal Identification Number
+pin = input("Please choose a 4 digit Personal Identification Number: ")
+
+print("\nThank you",firstName + ", we see that you set your PIN to",pin)
